761b21de0f567c153861f202b04fec735b4ae888
Kunjpatel2027 <122576496+Kunjpatel2027@users.noreply.github.com>
2024-05-22 22:10:33 -0400

    finding subsets with backtracking

--- backtracking.java
+++ backtracking.java
@@ -18,10 +18,31 @@ public class backtracking {
         System.out.println();
     }
 
+    // find subsets question
+    public static void findsubsets(String str, String ans, int i) {
+        // base case
+        if (i == str.length()) {
+            if (ans.length() == 0) {
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }
+        // recursion step
+
+        // Yes choice
+        findsubsets(str, ans + str.charAt(i), i + 1);
+        // no choice
+        findsubsets(str, ans, i + 1);
+    }
+
     public static void main(String[] args) {
+        String str = "Kunj";
+        findsubsets(str, "", 0);
         int arr[] = new int[5];
-        changeArr(arr, 0, 1);
-        printArr(arr);
+        // changeArr(arr, 0, 1);
+        // printArr(arr);
 
     }
 }
