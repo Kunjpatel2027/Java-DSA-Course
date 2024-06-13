5a1247e75688e820212bd7391927929ff8a8a46f
Kunjpatel2027 <122576496+Kunjpatel2027@users.noreply.github.com>
2024-06-13 12:24:32 -0400

    Interview Question of Container with the most water solved

--- DSAInterviewQuestions.java
+++ DSAInterviewQuestions.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class DSAInterviewQuestions {
     public static int trappedRainwater(int height[]) {
         int n = height.length;
@@ -45,11 +47,60 @@ public class DSAInterviewQuestions {
 
     }
 
-    public static void main(String[] args) {
+    // Array List interview Question : Container with most water
+    // public static int storeWater(ArrayList<Integer> height) {
+    // int maxWater = 0;
+    // // Brute Force Approach : O(n^2)
+    // for (int i = 0; i < height.size(); i++) {
+    // for (int j = i + 1; j < height.size(); j++) {
+    // int heightCont = Math.min(height.get(i), height.get(j));
+    // int widthCont = j - 1;
+    // int currWater = heightCont * widthCont;
+    // maxWater = Math.max(maxWater, currWater);
+    // }
 
-        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
-        int prices[] = { 7, 1, 5, 3, 6, 4 };
-        System.out.println(buyAndSellStocks(prices));
+    // }
+    // return maxWater;
+    // }
+
+    // 2 Pointer Approach : Time complexity: O(n)
+    public static int storeWaterNew(ArrayList<Integer> height) {
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.size() - 1;
+        while (lp < rp) {
+            // calculate Area of Water
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp - lp;
+            int currWater = ht * width;
+            maxWater = Math.max(maxWater, currWater);
+            // update Pointers
+            if (height.get(lp) < height.get(rp)) {
+                lp++;
+            } else {
+                rp--;
+            }
+        }
+        return maxWater;
+
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(6);
+        System.out.println(storeWaterNew(height));
+
+        // int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+        // int prices[] = { 7, 1, 5, 3, 6, 4 };
+        // System.out.println(buyAndSellStocks(prices));
         // System.out.println(trappedRainwater(height));
     }
 }
