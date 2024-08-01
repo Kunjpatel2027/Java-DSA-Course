fca946397c6ef4be1b7eb5170acb9f73d5cf82cf
Kunjpatel2027 <122576496+Kunjpatel2027@users.noreply.github.com>
2024-07-31 23:06:14 -0400

    check if palindrome in Linked list

--- LinkedList.java
+++ LinkedList.java
@@ -201,6 +201,52 @@ public class LinkedList {
 
     }
 
+    public Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next; // +1
+            fast = fast.next.next; // +2
+
+        }
+        return slow; // slow is my midnode
+    }
+
+    public boolean checkPalindrome() {
+        // base case
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        // step 1 : find mid node
+        Node midNode = findMid(head);
+
+        // step 2 : reverse 2nd half
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+        }
+        Node right = prev;
+        Node left = head;
+        // step 3: check if 1st half == 2nd half
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
 
@@ -208,7 +254,7 @@ public class LinkedList {
 
         ll.addFirst(1);
 
-        ll.addLast(4);
+        ll.addLast(2);
 
         ll.addLast(5);
         ll.addMiddle(2, 3);
@@ -222,8 +268,9 @@ public class LinkedList {
 
         // System.out.println(ll.recursiveSearch(3));
         // ll.reverseLL();
-        ll.deleteNthFromEnd(1);
-        ll.print();
+        // ll.deleteNthFromEnd(1);
+        System.out.println(ll.checkPalindrome());
+        // ll.print();
 
     }
 }
