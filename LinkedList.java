fe0c603445fd21ce4a90d10c0edab05eb4b90a32
Kunjpatel2027 <pate1037@algonquinlive.com>
2025-05-07 20:09:36 -0400

    Doubly Linked List and Add first function

--- LinkedList.java
+++ LinkedList.java
@@ -351,17 +351,63 @@ public class LinkedList {
 
     }
 
+    // ZigZag in LL (hint : )
+    public void zigZag() {
+        // find mid node
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+        // reverse 2nd Half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        // Alternate Merging
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+            left = nextL;
+            right = nextR;
+        }
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.addFirst(1);
-        ll.addFirst(2);
-        ll.addFirst(3);
-        ll.addFirst(4);
-        ll.addFirst(5);
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.print();
+        ll.zigZag();
+        ll.print();
+
+        // ll.addFirst(1);
+        // ll.addFirst(2);
+        // ll.addFirst(3);
+        // ll.addFirst(4);
+        // ll.addFirst(5);
         // 5>4>3>2>1
-        ll.print();
-        ll.head = ll.mergeSort(ll.head);
-        ll.print();
+        // ll.print();
+        // ll.head = ll.mergeSort(ll.head);
+        // ll.print();
 
         // head = new Node(1);
         // Node temp = new Node(2);
